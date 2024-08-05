c328c39708490c34d242a847c4415eb7b31355f3
baxromxoja <b.sultankhodjaev@baik.uz>
2024-08-05 16:36:51 +0500

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,34 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-
   entry: "./src/index.tsx", // Ensure this points to your TypeScript entry file
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"], // Include TypeScript extensions
+    path: path.resolve(__dirname, "dist"), // Output directory
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/, // Test for .ts and .tsx files
-        exclude: /node_modules/,
-        use: "ts-loader", // Use ts-loader to handle TypeScript files
-      },
-      {
-        test: /\.(js|jsx)$/, // Test for .js and .jsx files
-        exclude: /node_modules/,
-        use: "babel-loader", // Use babel-loader for JS/JSX files (if needed)
-      },
-      {
-        test: /\.css$/, // Rule for CSS files
+        test: /\.scss$/i,
         use: [
           "style-loader", // Injects styles into DOM
-          "css-loader", // Translates CSS into CommonJS modules
-        ],
-      },
-      {
-        test: /\.scss$/, // Rule for SCSS files
-        use: [
-          "style-loader", // Injects styles into DOM
-          "css-loader", // Translates CSS into CommonJS modules
+          "css-loader", // Turns CSS into CommonJS
           "sass-loader", // Compiles Sass to CSS
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
+          },
+        ],
+      },
+      // Additional rules can go here (e.g., for JavaScript, images, etc.)
     ],
   },
+  // Additional configurations such as plugins, mode, etc., can be added here
 };
